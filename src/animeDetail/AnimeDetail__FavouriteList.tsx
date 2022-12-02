0496d007a09e7744027bac33b14ebf11e0f1b207
dannycahyo <dannydwicahyono@gmail.com>
2022-12-02 08:18:19 +0700

    delete unusued modules

--- src/animeDetail/AnimeDetail__FavouriteList.tsx
+++ src/animeDetail/AnimeDetail__FavouriteList.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
-import { renderComponentOrNull } from "@utils/common/rendering";
 import useFavouriteAnimeStore from "@hooks/useFavouriteAnimeStore";
 
 namespace Caption {
